4de6bd5fa30a6b08dbee85662c6f0f3217d83d06
JackGamesFTW <28160910+JackGamesFTW@users.noreply.github.com>
2020-01-15 14:32:13 +0000

    Test fix for Icons on Laravel Vapor

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -5,8 +5,8 @@
     </head>
     <body class="env env--{{ app()->environment() }} @yield('appTypeClass')">
         <div class="svg-sprite">
-            {!! File::exists(public_path("/assets/admin/icons/icons.svg")) ? File::get(public_path("/assets/admin/icons/icons.svg")) : '' !!}
-            {!! File::exists(public_path("/assets/admin/icons/icons-files.svg")) ? File::get(public_path("/assets/admin/icons/icons-files.svg")) : '' !!}
+            {{ asset('/assets/admin/icons/icons.svg')  }}
+            {{ asset('/assets/admin/icons/icons-files.svg') }}
         </div>
         @if(config('twill.enabled.search', false))
             @partialView(($moduleName ?? null), 'navigation._overlay_navigation', ['search' => true])
